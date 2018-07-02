59b879a7d2f537c70d46d97e8bca1aaeec964c2b
sksamuel <sam@sksamuel.com>
2018-07-02 18:17:42 +0100

    Updated (de)serializer to declare exceptions

--- src/main/java/com/landoop/lenses/lsql/serde/Deserializer.java
+++ src/main/java/com/landoop/lenses/lsql/serde/Deserializer.java
@@ -3,7 +3,8 @@ package com.landoop.lenses.lsql.serde;
 import org.apache.avro.generic.GenericRecord;
 
 import java.io.Closeable;
+import java.io.IOException;
 
 public interface Deserializer extends Closeable {
-    GenericRecord deserialize(byte[] bytes);
+    GenericRecord deserialize(byte[] bytes) throws IOException;
 }
--- src/main/java/com/landoop/lenses/lsql/serde/Serializer.java
+++ src/main/java/com/landoop/lenses/lsql/serde/Serializer.java
@@ -3,7 +3,8 @@ package com.landoop.lenses.lsql.serde;
 import org.apache.avro.generic.GenericRecord;
 
 import java.io.Closeable;
+import java.io.IOException;
 
 public interface Serializer extends Closeable {
-    byte[] serialize(GenericRecord record);
+    byte[] serialize(GenericRecord record) throws IOException;
 }
