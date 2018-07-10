10c90e55cdc037de7ddb4a8956b4c51c9f41278f
stefan bocutiu <stefan.bocutiu@gmail.com>
2018-07-10 19:00:58 +0100

    remove topic from the interface

--- src/main/java/com/landoop/lenses/lsql/serde/Serde.java
+++ src/main/java/com/landoop/lenses/lsql/serde/Serde.java
@@ -1,9 +1,13 @@
 package com.landoop.lenses.lsql.serde;
 
+import org.apache.avro.Schema;
+
 import java.util.Properties;
 
 public interface Serde {
-    Serializer serializer(String topic, Properties properties);
+  Serializer serializer(Properties properties);
 
-    Deserializer deserializer(String topic, Properties properties);
+  Deserializer deserializer(Properties properties);
+
+  Schema getSchema();
 }
